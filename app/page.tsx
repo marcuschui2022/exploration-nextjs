060708e8bf6c096182f5fc98ea63222771ced25b
marcus <marcus.chui.ca@icloud.com>
2024-09-14 04:05:29 -0400

    feat: update navigation, layout, and dependencies
    - Removed Navigation component from Home page.
    - Added Home link to Navigation component.
    - Updated metadata in layout.
    - Integrated Navigation and footer in layout.
    - Added eslint, prettier, nodemon to devDependencies.

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 import Link from "next/link";
-import Navigation from "@/app/components/Navigation";
 
 export default function Home() {
-    return (
-        <div>
-            <Navigation/>
-            <h1>The Wild Oasis. Welcome to paradise.</h1>
+  return (
+    <div>
+      <h1>The Wild Oasis. Welcome to paradise.</h1>
 
-            <Link href="/cabins">Explore luxury cabins</Link>
-        </div>
-    )
+      <Link href="/cabins">Explore luxury cabins</Link>
+    </div>
+  );
 }
