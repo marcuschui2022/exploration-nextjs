ab76c24948e5a57721ee31dfd00028468fb076ac
marcus <marcus.chui.ca@icloud.com>
2024-09-14 04:20:39 -0400

    feat: Add a header logo component and integrate into the layout
    - Include a new Logo component in the header of layout.tsx
    - Add logo image (public/logo.png)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navigation from "@/app/components/Navigation";
 import { Metadata } from "next";
+import Logo from "@/app/components/Logo";
 
 export const metadata: Metadata = {
   title: "The Wild Oasis",
@@ -15,7 +16,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Navigation />
+        <header>
+          <Logo />
+          <Navigation />
+        </header>
         <main>{children}</main>
         <footer>Copyright by The Wild Oasis</footer>
       </body>
