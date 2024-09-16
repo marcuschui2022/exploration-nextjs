2bf9025a42688f35846f0b87ba4a62ddf110bb4c
marcus <marcus.chui.ca@icloud.com>
2024-09-16 02:52:33 -0400

    feat: enhance the home page with background image and improved styles
    - Added background image to home page
    - Enhanced styling for header and link buttons on home page
    - Updated layout body with relative positioning

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,8 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-        <body className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}>
+        <body
+            className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}>
         <Header/>
 
         <div className="flex-1 px-8 py-12">
