40b584e5b495ff62d1f566e2a4eaed9f9786745f
marcus <marcus.chui.ca@icloud.com>
2024-09-16 04:21:45 -0400

    feat: add user profile and reservation pages
    - Introduced `SideNavigation` and `SignOutButton` components.
    - Updated layout structure to include `SideNavigation`.
    - Added `SelectCountry` component for streamlined country selection.
    - Implemented new pages for managing user profile and reservations.
    - Integrated `ReservationCard` and `DeleteReservation` components.
    - Enhanced package.json with `date-fns`.

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,13 +1,15 @@
-import {Metadata} from "next";
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
-    title: "Guest area",
+  title: "Guest area",
 };
 
 export default function Page() {
-    return (
-        <div>
-            <h1>Account page</h1>
-        </div>
-    );
+  return (
+    // <div>
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+      Welcome, Marcus
+    </h2>
+    // </div>
+  );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,38 @@
 import React from "react";
-import {Metadata} from "next";
-import {Josefin_Sans} from "next/font/google";
+import { Metadata } from "next";
+import { Josefin_Sans } from "next/font/google";
 
-const josefin = Josefin_Sans({subsets: ['latin'], display: 'swap'})
+const josefin = Josefin_Sans({ subsets: ["latin"], display: "swap" });
 
-import '@/app/_styles/globals.css'
+import "@/app/_styles/globals.css";
 import Header from "@/app/_components/Header";
 
 export const metadata: Metadata = {
-    title: {
-        template: "%s | The Wild Oasis",
-        default: "Welcome | The Wild Oasis",
-    },
-    description: "Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests",
+  title: {
+    template: "%s | The Wild Oasis",
+    default: "Welcome | The Wild Oasis",
+  },
+  description:
+    "Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests",
 };
 
 export default function RootLayout({
-                                       children,
-                                   }: {
-    children: React.ReactNode;
+  children,
+}: {
+  children: React.ReactNode;
 }) {
-    return (
-        <html lang="en">
-        <body
-            className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}>
-        <Header/>
+  return (
+    <html lang="en">
+      <body
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
+      >
+        <Header />
 
-        <div className="flex-1 px-8 py-12">
-            <main className="max-w-7xl mx-auto">{children}</main>
+        <div className="flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl mx-auto w-full">{children}</main>
         </div>
         {/*<footer>Copyright by The Wild Oasis</footer>*/}
-        </body>
-        </html>
-    );
+      </body>
+    </html>
+  );
 }
