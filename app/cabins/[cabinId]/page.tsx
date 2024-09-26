188b8b15b434be07dc49e7772c455aff46b04456
marcus <marcus.chui.ca@icloud.com>
2024-09-26 06:00:57 -0400

    feat: add TextExpander component to cabin description
    - Introduced `TextExpander` component to handle expandable cabin descriptions.
    - Updated cabin detail page to use `TextExpander` for better UX.

--- app/cabins/[cabinId]/page.tsx
+++ app/cabins/[cabinId]/page.tsx
@@ -1,6 +1,7 @@
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import { getCabin, getCabins } from "@/app/_lib/data-service";
 import Image from "next/image";
+import TextExpander from "@/app/_components/TextExpander";
 
 // MOCK DATA
 // const cabin = {
@@ -66,7 +67,9 @@ export default async function Page({ params: { cabinId } }: PageProps) {
             Cabin {name}
           </h3>
 
-          <p className="text-lg text-primary-300 mb-10">{description}</p>
+          <p className="text-lg text-primary-300 mb-10">
+            <TextExpander>{description}</TextExpander>
+          </p>
 
           <ul className="flex flex-col gap-4 mb-7">
             <li className="flex gap-3 items-center">
