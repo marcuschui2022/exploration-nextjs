f66f41cfc497dc5634b547917a594eb246ef0c6f
marcus <marcus.chui.ca@icloud.com>
2024-09-27 01:45:10 -0400

    docs(Filter): add JSDoc comments to Filter's Button component

--- app/_components/Filter.tsx
+++ app/_components/Filter.tsx
@@ -55,6 +55,16 @@ export default function Filter() {
   );
 }
 
+/**
+ * Button component to render a styled button based on the active filter.
+ *
+ * @param {Object} params - The input parameters.
+ * @param {string} params.filter - The filter string that the button represents.
+ * @param {function} params.handleFilter - The function to call when the button is clicked with the filter string.
+ * @param {string} params.activeFilter - The currently active filter to determine button styling.
+ * @param {React.ReactNode} params.children - The content to be displayed inside the button.
+ * @return {React.ReactElement} The rendered button element.
+ */
 function Button({
   filter,
   handleFilter,
@@ -65,7 +75,7 @@ function Button({
   handleFilter: (filter: string) => void;
   activeFilter: string;
   children: React.ReactNode;
-}) {
+}): React.ReactElement {
   return (
     <button
       className={`px-5 py-2 hover:bg-primary-700 ${filter === activeFilter ? "bg-primary-700 text-primary-50" : ""}`}
